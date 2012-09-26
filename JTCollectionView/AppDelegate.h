9f859b702a086fe85cc17bea5650c913239c8b49
joycytao <ctao01.poly@gmail.com>
2012-09-26 15:59:06 -0500

    Initial Commit

---
+++ JTCollectionView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JTCollectionView
+//
+//  Created by Joy Tao on 9/26/12.
+//  Copyright (c) 2012 Joy Tao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JTCollectionView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JTCollectionView
+//
+//  Created by Joy Tao on 9/26/12.
+//  Copyright (c) 2012 Joy Tao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
